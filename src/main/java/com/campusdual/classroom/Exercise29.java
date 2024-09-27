ba9676c4f8c66003606fa87420400b658f84f5e4
Danielrey99 <danielreyperez99@gmail.com>
2024-09-27 14:13:54 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,12 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
+        int[] numeros = {1, 2, 3};
 
+        try {
+            int valor = numeros[5];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("El al acceder a index: " + e.getMessage());
+        }
     }
 }
